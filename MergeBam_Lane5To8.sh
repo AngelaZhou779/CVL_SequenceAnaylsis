107b2d415808fc6d0c138b4a805c6a615265c7ef
srmarzec <srmarzec@users.noreply.github.com>
2017-04-18 09:47:07 -0400

    Update MergeBam_Lane5To8.sh

--- MergeBam_Lane5To8.sh
+++ MergeBam_Lane5To8.sh
@@ -6,11 +6,11 @@ project_dir=/home/sarahm/cvl
 bam_dir=${project_dir}/storage/bam_dir
 merged=${project_dir}/storage/merged
 k
-files=(${bam_dir}/*_L001_aligned_pe.bam)
+files=(${bam_dir}/*_L005_aligned_pe.bam)
 for file in ${files[@]}
 do
 name=${file}
-base=`basename ${name} _L001_aligned_pe.bam`
+base=`basename ${name} _L005_aligned_pe.bam`
 samtools merge ${merged}/${base}_merged_aligned.bam \
   ${bam_dir}/${base}_L005_aligned_pe.bam \
   ${bam_dir}/${base}_L005_R1_aligned_se.bam \
