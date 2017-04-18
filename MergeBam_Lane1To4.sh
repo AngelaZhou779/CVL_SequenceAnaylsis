85d34f193dea7f87c7f145e7b4217e27d3e8109d
srmarzec <srmarzec@users.noreply.github.com>
2017-04-18 09:57:55 -0400

    Update MergeBam_Lane1To4.sh

--- MergeBam_Lane1To4.sh
+++ MergeBam_Lane1To4.sh
@@ -5,7 +5,7 @@ project_dir=/home/sarahm/cvl
 
 bam_dir=${project_dir}/storage/bam_dir
 merged=${project_dir}/storage/merged
-k
+
 files=(${bam_dir}/*_L001_aligned_pe.bam)
 for file in ${files[@]}
 do
