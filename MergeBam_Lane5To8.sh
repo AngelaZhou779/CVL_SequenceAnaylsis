82f9d1c4dc7fc9ef08e73ddf2c120fcd528da07c
srmarzec <srmarzec@users.noreply.github.com>
2017-04-18 09:39:13 -0400

    Create MergeBam_Lane5To8.sh

---
+++ MergeBam_Lane5To8.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+project_name=cvl
+project_dir=/home/sarahm/cvl
+
+bam_dir=${project_dir}/storage/bam_dir
+merged=${project_dir}/storage/merged
+k
+files=(${bam_dir}/*_L001_aligned_pe.bam)
+for file in ${files[@]}
+do
+name=${file}
+base=`basename ${name} _L001_aligned_pe.bam`
+samtools merge ${merged}/${base}_merged_aligned.bam \
+  ${bam_dir}/${base}_L005_aligned_pe.bam \
+  ${bam_dir}/${base}_L005_R1_aligned_se.bam \
+  ${bam_dir}/${base}_L005_R2_aligned_se.bam \
+  ${bam_dir}/${base}_L006_aligned_pe.bam \
+  ${bam_dir}/${base}_L006_R1_aligned_se.bam \
+  ${bam_dir}/${base}_L006_R2_aligned_se.bam \
+  ${bam_dir}/${base}_L007_aligned_pe.bam \
+  ${bam_dir}/${base}_L007_R1_aligned_se.bam \
+  ${bam_dir}/${base}_L007_R2_aligned_se.bam \
+  ${bam_dir}/${base}_L008_aligned_pe.bam \
+  ${bam_dir}/${base}_L008_R1_aligned_se.bam \
+  ${bam_dir}/${base}_L008_R2_aligned_se.bam
+done
