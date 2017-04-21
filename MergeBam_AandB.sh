a4b798c628966faf42aeb867a6a2ea223c45fc41
srmarzec <srmarzec@users.noreply.github.com>
2017-04-21 09:50:13 -0400

    Update MergeBam_AandB.sh

--- MergeBam_AandB.sh
+++ MergeBam_AandB.sh
@@ -4,6 +4,7 @@ project_name=cvl
 project_dir=/home/sarahm/cvl
 
 merged=${project_dir}/storage/merged
+merged_all=${project_dir}/storage/merged_all
 
 files=(${merged}/A_*)
 for file in ${files[@]}
@@ -11,7 +12,7 @@ do
 name=${file}
 base=`basename ${name} _merged_aligned.bam`
 new_base=${base:2}
-samtools merge ${merged}/${new_base}_merged_all_aligned.bam \
+samtools merge ${merged_all}/${new_base}_merged_all_aligned.bam \
   ${merged}/A_${new_base}_merged_aligned.bam \
   ${merged}/B_${new_base}_merged_aligned.bam
 done
