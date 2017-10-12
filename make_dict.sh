1ae5714944752f7cad4de569028e44039e14a2f7
srmarzec <srmarzec@users.noreply.github.com>
2017-10-12 13:45:12 -0400

    Update make_dict.sh

--- make_dict.sh
+++ make_dict.sh
@@ -1,7 +1,7 @@
-   #! /bin/bash
+#! /bin/bash
 
-   pic=/usr/local/picard-tools-1.131/picard.jar
-   index_dir=/home/sarahm/cvl/index_dir
-   ref_genome=${index_dir}/dmel-all-chromosome-r5.57_2.fasta
+pic=/usr/local/picard-tools-1.131/picard.jar
+index_dir=/home/sarahm/cvl/index_dir
+ref_genome=${index_dir}/dmel-all-chromosome-r5.57_2.fasta
 
-   java -jar ${pic} CreateSequenceDictionary R=${ref_genome} O=${index_dir}/dmel-all-chromosome-r5.57_2.dict
+java -jar ${pic} CreateSequenceDictionary R=${ref_genome} O=${index_dir}/dmel-all-chromosome-r5.57_2.dict
